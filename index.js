a723d630d8312d6de49c9a7b2fd40f008d4e770f
Johannes Braun <Braun-Johannes@outlook.de>
2024-04-22 14:54:02 +0200

    ja

--- index.js
+++ index.js
@@ -11,46 +11,41 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 42;
+let maxPage = 1;
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
 
-export async function fetchCharacters(pageIndex) {
+function renderCharacterCards(characters) {
   cardContainer.innerHTML = " ";
+  characters.forEach((character) => {
+    const { name, status, episode, type, image } = character;
+    const occurrences = episode.length;
+
+    CharacterCard(name, status, type, occurrences, image);
+  });
+}
+async function fetchCharacters(pageIndex) {
   const response = await fetch(
     `https://rickandmortyapi.com/api/character?page=${pageIndex}`
   );
   const data = await response.json();
+  maxPage = data.info.pages;
   const characters = data.results;
-  characters.forEach((character) => {
-    const {
-      name: name,
-      status: status,
-      episode: episodes,
-      type: type,
-      image: url,
-    } = character;
-    const occurrences = episodes.length;
-
-    CharacterCard(name, status, type, occurrences, url);
-  });
-  // console.log(data);
+  renderCharacterCards(characters);
 }
 
-fetchCharacters();
-function buttonListener() {
-  prevButton.addEventListener("click", () => {
-    let nextPageIndex = Math.max(page - 1, 1);
-    fetchCharacters(nextPageIndex);
-    page = nextPageIndex;
-    pagination.textContent = page + "/42";
-  });
+fetchCharacters(1);
 
-  nextButton.addEventListener("click", () => {
-    let nextPageIndex = Math.min(page + 1, 42);
-    fetchCharacters(nextPageIndex);
-    page = nextPageIndex;
-    pagination.textContent = page + "/42";
-  });
-}
-buttonListener();
+prevButton.addEventListener("click", () => {
+  const nextPageIndex = Math.max(page - 1, 1);
+  fetchCharacters(nextPageIndex);
+  page = nextPageIndex;
+  pagination.textContent = page + `/${maxPage}`;
+});
+
+nextButton.addEventListener("click", () => {
+  const nextPageIndex = Math.min(page + 1, maxPage);
+  fetchCharacters(nextPageIndex);
+  page = nextPageIndex;
+  pagination.textContent = page + `/${maxPage}`;
+});
