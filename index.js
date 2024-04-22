3bc6cf22e113f5c977dd4606840847502f729225
winget <philip.rausch@gmail.com>
2024-04-22 10:53:50 +0200

    Initial commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+
 // States
 const maxPage = 1;
 const page = 1;
