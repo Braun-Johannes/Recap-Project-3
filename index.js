00f455a2af9b645147659d26fae9a6ac5b7cc102
winget <philip.rausch@gmail.com>
2024-04-22 16:19:32 +0200

    Add first version of character search

--- index.js
+++ index.js
@@ -24,28 +24,70 @@ function renderCharacterCards(characters) {
     CharacterCard(name, status, type, occurrences, image);
   });
 }
-async function fetchCharacters(pageIndex) {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${pageIndex}`
-  );
+async function fetchCharacters(pageIndex, searchQuery) {
+  let targetUrl = "https://rickandmortyapi.com/api/character";
+
+  console.log(targetUrl);
+
+  if (pageIndex !== null) {
+    targetUrl = targetUrl + `?page=${pageIndex}`;
+    console.log(targetUrl);
+  }
+
+  if (searchQuery !== null) {
+    if (pageIndex === null) {
+      targetUrl = targetUrl + `?name=${searchQuery}`;
+    } else {
+      targetUrl = targetUrl + `&name=${searchQuery}`;
+    }
+    console.log(targetUrl);
+  }
+
+  const response = await fetch(targetUrl);
+
   const data = await response.json();
+
+  console.log(data.info.pages);
+
   maxPage = data.info.pages;
   const characters = data.results;
+
+  console.log(characters);
+
   renderCharacterCards(characters);
 }
 
-fetchCharacters(1);
+fetchCharacters(1, null);
 
 prevButton.addEventListener("click", () => {
   const nextPageIndex = Math.max(page - 1, 1);
-  fetchCharacters(nextPageIndex);
+  fetchCharacters(nextPageIndex, searchQuery);
   page = nextPageIndex;
   pagination.textContent = page + `/${maxPage}`;
 });
 
 nextButton.addEventListener("click", () => {
   const nextPageIndex = Math.min(page + 1, maxPage);
-  fetchCharacters(nextPageIndex);
+  fetchCharacters(nextPageIndex, searchQuery);
   page = nextPageIndex;
   pagination.textContent = page + `/${maxPage}`;
+});
+
+searchBar.addEventListener("submit", () => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  searchQuery = data.query;
+  page = 1;
+
+  if (searchQuery !== "") {
+    fetchCharacters(page, searchQuery);
+    pagination.textContent = `${page}/${maxPage}`;
+
+    console.log(maxPage);
+  } else {
+    searchQuery = "";
+    fetchCharacters(page, searchQuery);
+  }
+  console.log("Query: ", data.query);
 });
